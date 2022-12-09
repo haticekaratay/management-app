b6f70ac3955dc1f608df8f2ee5252eda205dd0fd
Hatice Karatay <haticekaraty@gmail.com>
2022-12-08 19:25:29 -0500

    initial commit

---
+++ backend/src/test/java/com/karatay/lightfeather/LightfeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karatay.lightfeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightfeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
